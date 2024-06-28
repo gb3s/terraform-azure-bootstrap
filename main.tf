a2e705302b718ef6c3ec934874d10c60dda1877f
geborgeson <46825704+geborgeson@users.noreply.github.com>
2024-06-28 07:22:00 +0000

    adjust resource group name var

--- main.tf
+++ main.tf
@@ -1,10 +1,10 @@
-resource "azurerm_resource_group" "bootsrap" {
-  name     = var.name
+resource "azurerm_resource_group" "bootstrap" {
+  name     = azurerm_resource_group.bootstrap.name
   location = var.location
 }
 
 resource "azurerm_user_assigned_identity" "cluster_id" {
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.bootstrap.name
   location            = var.location
 
   name = "${var.name}-cluster"
@@ -25,7 +25,7 @@ resource "azurerm_role_assignment" "cluster_group_role_assignment" {
 resource "azurerm_virtual_network" "network" {
   name                = "${var.name}-network"
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.bootstrap.name
   address_space       = var.network.address_space
 }
 
@@ -37,32 +37,32 @@ resource "azurerm_role_assignment" "sub_read_role_assignment" {
 
 resource "azurerm_route_table" "cluster" {
   name                = "${var.name}-route-table"
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.bootstrap.name
   location            = var.location
 }
 
-resource "azurerm_subnet" "agentnet" {
+resource "azurerm_subnet" "system_net" {
   name                 = "agent-nodepool"
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = azurerm_resource_group.bootstrap.name
   virtual_network_name = azurerm_virtual_network.network.name
   address_prefixes     = ["10.0.4.0/24"]
 }
 
-resource "azurerm_subnet" "agentnet2" {
+resource "azurerm_subnet" "agentnet" {
   name                 = "agent-nodepool02"
-  resource_group_name  = var.resource_group_name
+  resource_group_name  = azurerm_resource_group.bootstrap.name
   virtual_network_name = azurerm_virtual_network.network.name
   address_prefixes     = ["10.0.8.0/24"]
 }
 
 
 resource "azurerm_subnet_route_table_association" "system-rt-asc" {
-  subnet_id      = azurerm_subnet.agentnet.id
+  subnet_id      = azurerm_subnet.system_net.id
   route_table_id = azurerm_route_table.cluster.id
 }
 
 resource "azurerm_subnet_route_table_association" "agent-rt-asc" {
-  subnet_id      = azurerm_subnet.agentnet2.id
+  subnet_id      = azurerm_subnet.agentnet.id
   route_table_id = azurerm_route_table.cluster.id
 }
 
@@ -71,7 +71,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
   kubernetes_version  = var.cluster.kubernetes_version
   name                = var.name
   location            = var.location
-  resource_group_name = var.resource_group_name
+  resource_group_name = azurerm_resource_group.bootstrap.name
   dns_prefix          = "${var.name}-cluster"
   node_resource_group = "${var.name}-nodes"
   oidc_issuer_enabled = true
@@ -92,7 +92,7 @@ resource "azurerm_kubernetes_cluster" "cluster" {
     max_count           = 5
     enable_auto_scaling = true
     vm_size             = "Standard_B2s"
-    vnet_subnet_id      = azurerm_subnet.agentnet.id
+    vnet_subnet_id      = azurerm_subnet.system_net.id
   }
 
   network_profile {
@@ -122,7 +122,7 @@ resource "azurerm_kubernetes_cluster_node_pool" "agent" {
   max_count             = 1
   enable_auto_scaling   = true
 
-  vnet_subnet_id = azurerm_subnet.agentnet2.id
+  vnet_subnet_id = azurerm_subnet.agentnet.id
 }
 
 output "cluster_identity" {
